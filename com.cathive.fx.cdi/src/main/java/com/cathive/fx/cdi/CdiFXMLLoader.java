57804e7098bf2e82d84d6a1ff9e215a0327e8303
Benjamin P. Jung <headcr4sh@gmail.com>
2014-03-06 21:21:56 +0100

    Adjust CdiFXMLLoader to work with CDI and non-CDI-aware beans.
    This should make it possible to mix CDI-managed and unmanaged beans when constructing FXML files.

--- com.cathive.fx.cdi/src/main/java/com/cathive/fx/cdi/CdiFXMLLoader.java
+++ com.cathive.fx.cdi/src/main/java/com/cathive/fx/cdi/CdiFXMLLoader.java
@@ -5,6 +5,7 @@ import javafx.fxml.JavaFXBuilderFactory;
 import javafx.util.Builder;
 import javafx.util.BuilderFactory;
 
+import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.spi.CDI;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -22,11 +23,25 @@ public class CdiFXMLLoader extends FXMLLoader {
     public static final String RESOURCES_UNSPECIFIED = "$$$RESOURCES_UNSPECIFIED$$$";
     public static final String CHARSET_UNSPECIFIED = "$$$CHARSET_UNSPECIFIED$$$";
 
+    /**
+     * A reference to all the package names that won't work as supposed to when trying
+     * to instantiate beans from within via CDI in conjunction with FXML builders.
+     * <p>This will avoid an exception message caused by a {@link com.sun.javafx.fxml.PropertyNotFoundException}
+     * <em>'Property "${propertyName}" does not exist or is read-only'</em>.</p>
+     * @see com.sun.javafx.fxml.PropertyNotFoundException
+     */
+    private static final String[] CDI_BLACKLIST = new String[] {
+            "javafx",
+            "java.awt",
+            "javax.swing"
+    };
+
+    /** A default (non-CDI aware) JavaFX builder factory. */
+    private final JavaFXBuilderFactory defaultBuilderFactory;
 
     /**
      * Default (private) constructor.
-     * <p>Use the {@link #create(String, String, String, Class)} method instead
-     * to retrieve new instances of this class.</p>
+     * <p>Use the {@link #create(String, String, String, Class)} method to retrieve new instances of this class.</p>
      */
     private CdiFXMLLoader() {
 
@@ -36,8 +51,37 @@ public class CdiFXMLLoader extends FXMLLoader {
         // that have been specified via "fx:controller='...'" in our FXML files.
         this.setControllerFactory((aClass) -> CDI.current().select(aClass));
 
-        // Uses CDI to instantiate all components within our FXML files.
-        this.setBuilderFactory(aClass -> () -> CDI.current().select(aClass).get());
+        // Initializes the default JavaFX builder factory to be used for non CDI-aware beans.
+        this.defaultBuilderFactory = new JavaFXBuilderFactory();
+
+        this.setBuilderFactory((aClass) -> {
+
+            // Uses the default builder factory to retrieve builders where applicable.
+            final Builder<?> defaultBuilder = defaultBuilderFactory.getBuilder(aClass);
+            if (defaultBuilder != null) { return defaultBuilder; }
+
+            // Make sure that we use a "null" builder for components that are 'blacklisted'.
+            final String packageName = aClass.getPackage().getName();
+            for (final String blacklisted: CDI_BLACKLIST) {
+                if (packageName.startsWith(blacklisted)) {
+                    return null;
+                }
+            }
+
+            // Uses CDI to retrieve a bean of the desired class.
+            final Instance<?> cdiBean = CDI.current().select(aClass);
+            if (cdiBean != null && !cdiBean.isUnsatisfied()) {
+                return new Builder<Object>() {
+                    @Override
+                    public Object build() {
+                        return cdiBean.get();
+                    }
+                };
+            }
+
+            throw new IllegalStateException(String.format("Unable to construct bean of class %s", aClass.getName()));
+
+        });
 
     }
 
